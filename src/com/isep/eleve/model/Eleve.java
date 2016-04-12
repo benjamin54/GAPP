70216c1ac1079cc1d4b36c6ff22872b9ac14660b
benjamin54 <dubus.benjamin1@gmail.com>
2016-04-12 09:24:43 +0200

    finalisation organisation dossiers

--- src/com/isep/eleve/model/Eleve.java
+++ src/com/isep/eleve/model/Eleve.java
@@ -1,5 +1,28 @@
 package com.isep.eleve.model;
 
 public class Eleve {
-	public String eleve;
-}
+	    private String email;
+	    private String motDePasse;
+	    private String nom;
+
+	    public void setEmail(String email) {
+		this.email = email;
+	    }
+	    public String getEmail() {
+		return email;
+	    }
+
+	    public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	    }
+	    public String getMotDePasse() {
+		return motDePasse;
+	    }
+
+	    public void setNom(String nom) {
+		this.nom = nom;
+	    }
+	    public String getNom() {
+		return nom;
+	    }
+	}
