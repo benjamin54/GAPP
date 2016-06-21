a1e68e3a2a8c77bf7d7eeaa927010ae40c591920
MayeulSGC <mayeul.sgc@gmail.com>
2016-06-21 12:24:42 +0200

    fonction avec group

--- src/com/isep/metier/CompetencesgrpUtil.java
+++ src/com/isep/metier/CompetencesgrpUtil.java
@@ -7,21 +7,25 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class CompetencesgrpUtil {
-public ArrayList<Competencesgrp> chargerAbsenceByUser (Famillecompetencesgrp Fcmpt){
-		
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		Transaction tx=null;
-		List  results= null ;
-		try{
-			tx=session.beginTransaction();
-			int id=user.getIdUsers();
-			ArrayList<Absences> ab=(ArrayList<Absences>) session.createQuery("FROM Absences WHERE Users_idUsers ="+"'"+id+"'").list();
+public void creerCompetences(Competences comp){
+	
+}
 
-			return ab;
-		}catch(Exception e){
-			e.printStackTrace();
-			System.out.println("erreur fonction chargerUser()" );
-			return null;
-		}finally{}
-	}
+//public ArrayList<Competencesgrp> chargerAbsenceByUser (Famillecompetencesgrp Fcmpt){
+//		
+///		Session session = HibernateUtil.getSessionFactory().openSession();
+//		Transaction tx=null;
+//		List  results= null ;
+//		try{
+//			tx=session.beginTransaction();
+//			int id=user.getIdUsers();
+//			ArrayList<Absences> ab=(ArrayList<Absences>) session.createQuery("FROM Absences WHERE Users_idUsers ="+"'"+id+"'").list();
+//
+//			return ab;
+//		}catch(Exception e){
+//			e.printStackTrace();
+//			System.out.println("erreur fonction chargerUser()" );
+//			return null;
+//		}finally{}
+//	}
 }
